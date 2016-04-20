c4469f84f2fef2b26fe17e31e177ab819ba39e9e
Jeremy Bae <jswbae@gmail.com>
2016-04-21 00:05:31 +0900

    Changed domain

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.db = {};
-config.webhost = 'http://localhost:4569/';
+config.webhost = 'https://go.gagbits.com/';
 
 config.db.host = 'localhost';
 config.db.name = 'url_shortener';
