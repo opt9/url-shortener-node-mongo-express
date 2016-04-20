53dfb36e3ffa16a3ab93584a0a982861776f8de0
Jeremy Bae <jswbae@gmail.com>
2016-04-20 23:29:18 +0900

    Modified some code

--- app.js
+++ app.js
@@ -70,6 +70,6 @@ app.get('/:encoded_id', function(req, res){
 
 });
 
-var server = app.listen(3000, function(){
-  console.log('Server listening on port 3000');
+var server = app.listen(4569, function(){
+  console.log('Server listening on port 4569');
 });
--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.db = {};
-config.webhost = 'http://localhost:3000/';
+config.webhost = 'http://localhost:4569/';
 
 config.db.host = 'localhost';
 config.db.name = 'url_shortener';
